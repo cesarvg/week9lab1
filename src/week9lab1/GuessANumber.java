02a08ffd42723c2178803e83878a89d88e92f0b2
cesarvg <alexvg714@yahoo.com>
2015-04-14 20:38:11 -0700

    first commit

---
+++ src/week9lab1/GuessANumber.java
@@ -0,0 +1,18 @@
+package week9lab1;
+
+//import week9.cLightBulb;
+
+public class GuessANumber {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		RandomNumber n1;
+		n1 = new RandomNumber();
+		System.out.println (n1.thenumber() );
+		
+	}
+
+}
---
+++ src/week9lab1/RandomNumber.java
@@ -0,0 +1,15 @@
+package week9lab1;
+
+public class RandomNumber {
+	
+	
+private int randomnumber;
+	
+	public int thenumber() {
+		
+		return randomnumber = 1 + (int) ( Math.random() * 10 );
+		
+	}
+	
+
+}
